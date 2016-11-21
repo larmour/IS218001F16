5cd9f53f8f9d6b7549d5527ce7a54fa905e1c3f9
LindseyArmour <la95@njit.edu>
2016-11-21 15:13:57 -0500

    form validation

--- form_validation/formval.php
+++ form_validation/formval.php
@@ -6,61 +6,27 @@ class sanitize {
       return filter_var($_POST[$fieldname], FILTER_SANITIZE_EMAIL);
     }
   }
-  public function sanPassword($fieldname) {
-    if(isset($_POST[$fieldname])) {
-       return filter_var($_POST[$fieldname], FILTER_SANITIZE_PASSWORD);
-    }
-  }
-  public function sanURL($fieldname) {
-    if(isset($_POST[$fieldname])) {
-      return filter_var($_POST[$fieldname], FILTER_SANITIZE_URL);
-    }
-  }
 }
 class validate {
-  public function valEmail($fieldname) {
-    $email = filter_vat($_POST[$fieldname], FILTER_SANITIZE_EMAIL);
+  public function validateEmail($fieldname) {
+    $email = filter_var($_POST[$fieldname], FILTER_SANITIZE_EMAIL);
     if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
       return TRUE;
     } else {
       return FALSE ;
     }
    }
-   public function valPassword($fieldname) {
-     $password = filter_var($_POST[$fieldname], FILTER_SANITIZE_PASSWORD);
-     if(filter_var($password, FILTER_VALIDATE_PASSWORD)) {
-       return TRUE;
-     } else {
-       return FALSE;
-     }
-  }
-  public function valURL($fieldname) {
-    $url = filter_var($_POST[$fieldname], FILTER_SANTIZE_URL);
-    if(filter_var($email, FILTER_VALIDATE_URL)) {
-      return TRUE;
-    } else {
-      return FALSE;
-    }
-  }
 }
 //Sanitize the examples
-$san = new sanitize();
-echo $san->sanEmail('email');
-echo '<br>';
-echo $san->sanURL('url');
+$sanitize = new sanitize();
+echo $sanitize->sanitizeEmail('email');
+
 
 //validate the examples
-$val = new validate();
+$validate = new validate();
 echo '<br>';
-if($val->valEmail('email')) {
+if($validate->validateEmail('email')) {
   echo 'This email is valid';
 }
-echo '<br>';
-if($val->valPassword('password')) {
-  echo 'This password is valid';
-}
-echo '<br>';
-if($val->valURL('URL')) {
-  echo 'This URL is valid';
-}
+
 ?>
